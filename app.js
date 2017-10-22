d6b97606befbdeefa241cbc73004046402c58347
siberio2000 <siberio2000@gmail.com>
2017-10-22 19:43:32 +0100

    JSON

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_PbkZpEMdepxcoG9yGL7O3NuK');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middlware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })); //this is all in documentation for express handlebars
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index route
+app.get('/', (req, res) => {
+    res.render('index'); //we want to render template
+});
+
+//Charge Route
+app.post('/charge', (req, res) => {
+    const amount = 2500;
+
+    console.log(req.body);
+    return res.send('TEST');
+
+    // stripe.customers.create({
+    //         email: req.body.stripeEmail,
+    //         source: req.body.stripeToken
+    //     })
+    //     .then(customer => stripe.charges.create({
+    //         amount,
+    //         description: 'Web Development Ebook',
+    //         currency: 'GBP',
+    //         customer: customer.id
+    //     }))
+    //     .then(charge => res.render('success'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
